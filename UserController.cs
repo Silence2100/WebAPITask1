7f4ed9496494fa00c08bb6efe1a9b5d97d7aacd4
SILENCE2099 <mqwert1999@gmail.com>
2026-04-23 20:09:35 +0300

    Добавил запросы на изменение и удаление

--- UserController.cs
+++ UserController.cs
@@ -24,7 +24,7 @@
         [HttpGet("{id}")]
         public ActionResult<User> GetUserById(int id)
         {
-            var user = _users.FirstOrDefault(u => u.Id == id);
+            var user = FindUserById(id);
 
             if (user == null)
             {
@@ -37,6 +37,41 @@
             }
 
             return Ok(user);
+        }
+
+        [HttpPut("id")]
+        public ActionResult<User> UpdateUserName(int id, [FromBody] string newName)
+        {
+            var user = FindUserById(id);
+
+            if (user == null)
+            {
+                return NotFound($"Пользователь с id = {id} не найден");
+            }
+
+            user.Name = newName;
+
+            return Ok(user);
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = FindUserById(id);
+
+            if (user == null)
+            {
+                return NotFound($"Пользователь с id = {id} не найден");
+            }
+
+            _users.Remove(user);
+
+            return Ok($"Пользователь с id = {id} удалён");
+        }
+
+        private User? FindUserById(int id)
+        {
+            return _users.FirstOrDefault(u => u.Id == id);
         }
     }
 }
