24533431ccb82a9b5a26f819ebf06c35d8eb8011
SILENCE2099 <mqwert1999@gmail.com>
2026-04-23 19:03:15 +0300

    Добавил Get запросы

--- UserController.cs
+++ UserController.cs
@@ -14,5 +14,29 @@
             new User { Id = 4, Name = "Мария" },
             new User { Id = 5, Name = "Секретный пользователь" },
         };
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            return Ok(_users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound($"Пользователь с id = {id} не найден");
+            }
+
+            if (id == 5)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "Доступ к пользователю с id = 5 запрещён");
+            }
+
+            return Ok(user);
+        }
     }
 }
